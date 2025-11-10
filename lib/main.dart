ca8a162c34558a372c067e4d9258f2b58b42b8a7
李锦涛 <lijt@eetrust.com>
2025-11-10 18:02:43 +0800

    feat(master):layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_example/dialogs.dart';
-import 'package:flutter_example/animation.dart';
+import 'package:flutter_example/layout.dart';
 import 'package:flutter_example/navigators.dart';
+import 'package:flutter_example/uifield.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 void main() {
@@ -33,14 +34,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,37 +42,126 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          spacing: 20,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () => {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => AlertDialogExample()),
-                ),
-              },
-              child: Text('Dialog'),
+        child: Container(
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: 10),
+              child: ContentState(title: widget.title),
             ),
-            ElevatedButton(
-              onPressed: () => {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Navigators(title: widget.title),
-                  ),
-                ),
-              },
-              child: Text('Navigators'),
-            ),
-            ElevatedButton(
-              onPressed: () => {
-                launchUrlString('http://www.baidu.com',mode: LaunchMode.inAppWebView)
-              },
-              child: Text('loadUrl'),
-            ),
-          ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class ContentState extends StatelessWidget {
+  const ContentState({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.max,
+        // spacing: 20,
+        children: <Widget>[
+          OutlinedButton(
+            onPressed: () {
+
+            },
+            style: OutlinedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  UiField.ROUNDED_RECTANGLE_BORDER_RADIUS,
+                ),
+              ),
+            ),
+            child: Text('OutlinedButton'),
+          ),
+          FilledButton(
+            onPressed: () {},
+            style: FilledButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  UiField.ROUNDED_RECTANGLE_BORDER_RADIUS,
+                ),
+              ),
+            ),
+            child: Text('FilledButton'),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  UiField.ROUNDED_RECTANGLE_BORDER_RADIUS,
+                ),
+              ),
+              fixedSize: const Size.fromHeight(0),
+              backgroundColor: Colors.deepOrange,
+            ),
+            onPressed: () => {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => AlertDialogExample()),
+              ),
+            },
+            child: Text('Dialog', style: TextStyle(color: Colors.white)),
+          ),
+          ElevatedButton(
+            onPressed: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Navigators(title: title),
+                ),
+              ),
+            },
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  UiField.ROUNDED_RECTANGLE_BORDER_RADIUS,
+                ),
+              ),
+            ),
+            child: Text('Navigators'),
+          ),
+          ElevatedButton(
+            onPressed: () => {
+              launchUrlString(
+                'http://www.baidu.com',
+                mode: LaunchMode.inAppWebView,
+              ),
+            },
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  UiField.ROUNDED_RECTANGLE_BORDER_RADIUS,
+                ),
+              ),
+            ),
+            child: Text('loadUrl'),
+          ),
+          ElevatedButton(
+            onPressed: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LayoutState()),
+              ),
+            },
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  UiField.ROUNDED_RECTANGLE_BORDER_RADIUS,
+                ),
+              ),
+            ),
+            child: Text('layout'),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/uifield.dart
@@ -0,0 +1,8 @@
+/// Desc:
+/// Author: lijt
+/// Created on 2025/11/10
+/// Email: lijt@eetrust.com
+class UiField{
+  static const double ROUNDED_RECTANGLE_BORDER_RADIUS = 3.0;
+}
+ 
